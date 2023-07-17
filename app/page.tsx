183389400f94af9e5d5e68e432059f3d73897fdd
Beau Elliott <belliott1516@gmail.com>
2023-07-17 14:23:00 -0700

    getting layout set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Nav from "../components/Nav";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
-      <Nav />
-      <h1 className="">Beau Elliott</h1>
+    <main className="flex flex-col min-h-screen p-24 text-center">
+      <section className="flex flex-col justify-center w-full border">
+        <h1 className="text-5xl bg-gray-500">Beau Elliott</h1>
+        <h4>Full Stack Developer</h4>
+      </section>
+      <section>
+        Info
+      </section>
+      <section>
+        Projects
+      </section>
     </main>
   );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav className="flex justify-between">
+      <h1>logo</h1>
+      <p>Home</p>
+      <p>Projects</p>
+      <p>Cool Facts</p>
+      <p>contact me</p>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;

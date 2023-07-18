b69c8f17c2ad3fd8124b15c308be525045b46817
Beau Elliott <belliott1516@gmail.com>
2023-07-18 12:39:40 -0700

    setting up project mapping

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
+"use client";
+import Project from "@/components/Project";
+import { useEffect, useState } from "react";
+import projectData from "../data.jsx";
+
 export default function Home() {
+  const [projects, setProjects] = useState(projectData);
+
+  useEffect(() => {
+    console.log(projects, "projects");
+  }, []);
+
   return (
-    <main className="flex flex-col min-h-screen p-24 text-center">
+    <main className="flex flex-col p-24 text-center">
       <section className="flex flex-col justify-center w-full border">
         <h1 className="text-5xl bg-gray-500">Beau Elliott</h1>
         <h4>Full Stack Developer</h4>
       </section>
+      <section>Info</section>
       <section>
-        Info
-      </section>
-      <section>
-        Projects
+        {projects.map((item) => (
+          <Project key={item.id} {...item} />
+        ))}
       </section>
     </main>
   );

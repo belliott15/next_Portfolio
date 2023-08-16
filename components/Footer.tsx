4e14319302f904f2177c7f4f75eeb8da7d1f748d
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2023-08-16 14:44:28 -0700

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="flex">
-      <a href="">Contact Me</a>
-      <a>Info</a>
+    <section className="flex flex-col text-center">
+      <form className=" w-full">
+        <h2 className="text-2xl">Contact Me</h2>
+        <input
+          required
+          placeholder='"Your Email"'
+          className="pl-2 rounded"
+        ></input>
+        <textarea
+          placeholder="I would love to hear from you!"
+          className="p-2 h-40 m-4 rounded bg-white opacity-50 text-black"
+          required
+        ></textarea>
+      </form>
     </section>
   );
 };
